4abef6ee59d2063528ab1d47bf2f09fb4ba8baeb
Ash <acst52@gmail.com>
2023-03-09 19:55:08 -0500

    worked more on routes...

--- controllers/index.js
+++ controllers/index.js
@@ -9,3 +9,10 @@ router.use('/dashboard', dashboardRoutes);
 router.use('/api', apiRoutes);
 
 module.exports = router;
+
+
+// if user clicks button that has indian tag or if they search for indian, same route
+
+// or like there are cards (partials) that are randomly generated on the main view so the main view isnt boring and it offers suggestions to users who are uncertain or dont know what to search for. 
+
+// 
