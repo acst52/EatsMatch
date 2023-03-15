364f4f4964df33077261fb3227998292b082a933
MaximosSarmpas <117605647+MaximosSarmpas@users.noreply.github.com>
2023-03-15 15:22:41 -0400

    Update seed.js
    resto_promo added for each restaurant.

--- seeds/seed.js
+++ seeds/seed.js
@@ -2,8 +2,8 @@
 
 const { Resto, Dish } = require('./models');
 
-const doorDashData = require('./doorDashData.json');
-const uberEatsData = require('./uberEatsData.json');
+//const doorDashData = require('./doorDashData.json');
+//const uberEatsData = require('./uberEatsData.json');
 
 
 const seedRestaurants = async () => {
@@ -17,6 +17,7 @@ const seedRestaurants = async () => {
       phoneNumber: '(647) 748-8100',
       deliveryServices: ['DoorDash', 'Uber Eats'],
       rating: 4.5,
+      resto_promo: 'any value'
       resto_img: 'https://media-cdn.tripadvisor.com/media/photo-s/0a/2f/09/24/burger-s-priest.jpg',
       resto_dish: [
       { dishName: 'The Priest Burger', price: 8.99 },
@@ -30,6 +31,7 @@ const seedRestaurants = async () => {
       phoneNumber: '(323) 297-0101',
       deliveryServices: ['DoorDash', 'Uber Eats'],
       rating: 4.5,
+      resto_promo: 'any value'  
       resto_img: 'https://www.pizzeriamozza.com/media/2366/salumi_pizza_small.jpg',
       resto_dish: [
       { dishName: 'Margherita Pizza', price: 12.99 },
@@ -43,6 +45,7 @@ const seedRestaurants = async () => {
       phoneNumber: '(916) 781-3399',
       deliveryServices: ['DoorDash', 'Uber Eats'],
       rating: 4.5,
+      resto_promo: 'any value'  
       resto_img: 'https://www.thecheesecakefactory.com/assets/images/Menu-Import/CCF_Cheesecakes_Raspberry-Lemon_Cheesecake.jpg',
       resto_dish: [
       { dishName: 'Original Cheesecake', price: 7.99 },
@@ -56,6 +59,7 @@ const seedRestaurants = async () => {
         phoneNumber: '(714) 333-4688',
         deliveryServices: ['DoorDash', 'Uber Eats'],
         rating: 4.0,
+        resto_promo: 'any value'  
         resto_img: 'https://www.kurausa.com/wp-content/uploads/2020/05/Exterior_Anaheim.jpg',
         resto_dish: [
         {name: 'Salmon Nigiri', price: 2.25},
@@ -69,6 +73,7 @@ const seedRestaurants = async () => {
           phoneNumber: '(305) 374-4500',
           deliveryServices: ['DoorDash', 'Uber Eats'],
           rating: 4.5,
+          resto_promo: 'any value'  
           resto_img: 'https://www.thecapitalgrille.com/content/dam/tcg/menu/ToGo/The%20Capital%20Grille_Takeout_Menu_Holiday_Cheers_2021.jpg',
           resto_dish: [
           {name: 'Filet Mignon', price: 45.00},
@@ -82,6 +87,7 @@ const seedRestaurants = async () => {
           phonenumber: '(323) 380-9688',
           deliveryServices:['DoorDash', 'Uber Eats'],
           rating: 4.0,
+          resto_promo: 'any value'  
           resto_img: 'https://www.pfchangs.com/content/dam/pfchangs/en-US/Assets/01_Desktop/11_Menu/Dine-In/2018/Fall/Dine-In-Menu.png',
           resto_dish: [
         {name: 'Mongolian Beef ', price: 18.50},
@@ -96,6 +102,7 @@ const seedRestaurants = async () => {
         phoneNumber: '(401) 419-6357',
         deliveryServices:['Grubhub', 'Uber Eats'],
         rating: 4.0,
+        resto_promo: 'any value'  
         resto_img: 'https://www.thehalalguys.com/wp-content/uploads/2020/01/homepage-mobile-banner-2.jpg',
         resto_dish: [
         {name:'Chicken and Gyro Combo', price: 10.99},
@@ -109,6 +116,7 @@ const seedRestaurants = async () => {
        phoneNumber: '(281) 534-0873',
        deliveryServices: ['DoorDash', 'Uber Eats'],
        rating: 3.5,
+       resto_promo: 'any value'  
        resto_img: 'https://www.tgifridays.com/content/dam/fridays/images/menu/tgi-fridays-menu-1.png',
        resto_dish: [
      {name: 'Classic Cheeseburger', price: 12.49},
@@ -122,6 +130,7 @@ const seedRestaurants = async () => {
     phoneNumber: '(804) 360-4897',
     deliveryServices: ['DoorDash', 'Uber Eats'],
     rating: 4.5,
+    resto_promo: 'any value'  
     resto_img: 'https://www.fiveguys.com/assets/img/fg-logo.png',
     resto_dish: [
     {name: 'Bacon Cheeseburger',price: 8.99},
@@ -135,6 +144,7 @@ const seedRestaurants = async () => {
       phoneNumber: '(408) 960-0910',
       deliveryServices: ['DoorDash', 'Uber Eats'],
       rating: 4.0,
+      resto_promo: 'any value'  
       resto_img: 'https://www.blazepizza.com/wp-content/uploads/2021/01/Blaze_Hero_RGB.png',
     resto_dish: [
      {name: 'Build Your Own Pizza', price: 7.95},
@@ -148,6 +158,7 @@ const seedRestaurants = async () => {
     phoneNumber: '(702) 534-4494',
     deliveryServices: ['DoorDash', 'Uber Eats'],
     rating: 4.0,
+    resto_promo: 'any value'  
     resto_img: 'https://www.chipotle.com/content/dam/cmg-web/menu-page/CMG-Food-Banner_1440x640.jpg',
     resto_dish:  [
    {name: 'Chicken Burrito Bowl', price: 8.25},
@@ -161,6 +172,7 @@ const seedRestaurants = async () => {
    phoneNumber: '(212) 982-4540',
    deliveryServices: ['DoorDash', 'Uber Eats'],
    rating: 3.5,
+   resto_promo: 'any value'  
    resto_img: 'https://www.dunkindonuts.com/content/dunkindonuts/en/menu.html',
    resto_dish:  [
    {name: 'Bacon, Egg & Cheese Croissant',price: 4.69},
@@ -174,6 +186,7 @@ const seedRestaurants = async () => {
     phoneNumber: '(949) 661-1966',
     deliveryServices: ['DoorDash', 'Uber Eats'],
     rating: 4.0,
+    resto_promo: 'any value'  
     resto_img: 'https://www.meltingpot.com/content/dam/meltingpot/menus/main-menu.pdf',
     resto_dish:  [
     {name: 'Classic Alpine Cheese Fondue', price: 12.95},
@@ -187,6 +200,7 @@ const seedRestaurants = async () => {
     phoneNumber: '(407) 248-8800',
     deliveryServices: ['DoorDash', 'Uber Eats'],
     rating: 3.5,
+    resto_promo: 'any value'  
     resto_img: 'https://www.redlobster.com/content/dam/rl-assets/Menu/Online/February%202022%20Menu/RL_Menu_Online_February_2022.pdf',
     resto_dish:  [
     {name: 'Ultimate Feast', price: 36.99},
@@ -201,6 +215,7 @@ const seedRestaurants = async () => {
     phoneNumber: '(312) 932-9330',
     deliveryServices: ['DoorDash', 'Grubhub'],
     rating: 4.5,
+    resto_promo: 'any value'  
     resto_img: 'https://fogodechao.com/wp-content/uploads/2021/03/Fogo-Menu-10.2020.pdf',
     resto_dish:  [
     {name: 'Full Churrasco Experience', price: 54.95},
@@ -214,6 +229,7 @@ const seedRestaurants = async () => {
     phoneNumber: '(804) 782-1600',
     deliveryServices: ['DoorDash', 'Uber Eats'],
     rating: 3.5,
+    resto_promo: 'any value'  
     resto_img: 'https://www.kfc.com/_/items/images/menu/hero/KFC_9-Honey-Sauce-Chicken--1115.png',
     resto_dish:  [
     {name: '12-Piece Chicken Meal', price: 29.99},
@@ -227,6 +243,7 @@ const seedRestaurants = async () => {
     phoneNumber: '(501) 223-3355',
     deliveryServices: ['DoorDash', 'Uber Eats'],
     rating: 4.5,
+    resto_promo: 'any value'  
     resto_img: 'https://www.nandosperiperi.com/uploads/home/banners/Nandos-Peri-Peri-Banner-1-1920x960.jpg',
     resto_dish:  [
     {name: 'Half Chicken', price: 13.95},
@@ -240,6 +257,7 @@ const seedRestaurants = async () => {
    phoneNumber: '(812) 471-8600',
    deliveryServices: ['DoorDash', 'Uber Eats'],
    rating: 3.5,
+   resto_promo: 'any value'  
    resto_img: 'https://www.pandaexpress.com/images/website/global/products/large/KungPaoChicken.jpg',
    resto_dish:  [
    {name: 'Orange Chicken', price: 8.10},
@@ -254,6 +272,7 @@ const seedRestaurants = async () => {
    phoneNumber: '(310) 684-2889',
    deliveryServices: ['DoorDash', 'Uber Eats'],
    rating: 4.0,
+   resto_promo: 'any value'  
    resto_img: 'https://www.tatsuramen.com/wp-content/uploads/2021/07/logo_tatsu-1024x1024.png',
    resto_dish:  [
    {name: 'Tonkotsu Ramen', price: 11.95},
@@ -267,6 +286,7 @@ const seedRestaurants = async () => {
     phoneNumber: '(323) 634-9343',
     deliveryServices:  ['DoorDash', 'Uber Eats'],
     rating: 4.0,
+    resto_promo: 'any value'  
     resto_img: 'https://www.yardhouse.com/content/dam/restaurant/10914/menus/2022/02/18/yh-menu-02-18-22.pdf',
     resto_dish:  [
     {name: 'Nashville Hot Chicken', price: 17.75},
@@ -280,6 +300,7 @@ const seedRestaurants = async () => {
      phoneNumber: '(626) 683-3000',
      deliveryServices: ['DoorDash', 'Uber Eats'],
      rating: 4.5,
+     resto_promo: 'any value'  
     resto_img: 'https://www.sushiroku.com/wp-content/uploads/2019/12/Pasadena-1-1.jpg',
     resto_dish: [
 {name: 'Yellowtail Sashimi with Jalapeño', price: 23},
