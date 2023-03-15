090b96fb637552675dfda6de8e6ea49193d066b9
MaximosSarmpas <117605647+MaximosSarmpas@users.noreply.github.com>
2023-03-15 19:18:10 -0400

    Update seed.js
    some changes made

--- seeds/seed.js
+++ seeds/seed.js
@@ -309,50 +309,51 @@ const seedRestaurants = async () => {
    }
   ])   
     
-  const restaurants = await Resto.findAll();
-  console.log(restaurants)
-  for (let i = 0; i < restaurants.length; i++) {
-    const restaurant = restaurants[i];
-    console.log(restaurant.deliveryServices)
-    if (restaurant.deliveryServices.includes('DoorDash')) {
-      const doorDashRestaurant = doorDashData.find(
-        (r) => r.name.toLowerCase() === restaurant.name.toLowerCase()
-      );
+  // seedRestaurants(); 
+ const createDishes = async() => {
+ const restaurants = await Resto.findAll();
+ 
+console.log(restaurants)
+for (let i = 0; i < restaurants.length; i++) {
+  const restaurant = restaurants[i];
+  console.log(restaurant.deliveryServices)
+  if (restaurant.deliveryServices.includes('DoorDash')) {
+    const doorDashRestaurant = doorDashData.find(
+      (r) => r.name.toLowerCase() === restaurant.name.toLowerCase()
+    );
 
-      if (doorDashRestaurant) {
-        const dishes = doorDashRestaurant.menu_items.map((d) => ({
-          name: d.name,
-          price: d.price,
-          promotion: d.promotion,
-          deliveryService: 'DoorDash',
-          restaurantId: restaurant.id,
-        }));
+    if (doorDashRestaurant) {
+      const dishes = doorDashRestaurant.menu_items.map((d) => ({
+        name: d.name,
+        price: d.price,
+        promotion: d.promotion,
+        deliveryService: 'DoorDash',
+        restaurantId: restaurant.id,
+      }));
 
-        await Dish.bulkCreate(dishes);
-      }
-    }
-
-    if (restaurant.deliveryServices.includes('Uber Eats')) {
-      const uberEatsRestaurant = uberEatsData.find(
-        (r) => r.name.toLowerCase() === restaurant.name.toLowerCase()
-      );
-
-      if (uberEatsRestaurant) {
-        const dishes = uberEatsRestaurant.menu_items.map((d) => ({
-          name: d.name,
-          price: d.price,
-          promotion: d.promotion,
-          deliveryService: 'Uber Eats',
-          restaurantId: restaurant.id,
-        }));
-
-        await Dish.bulkCreate(dishes);
-      }
+      await Dish.bulkCreate(dishes);
     }
   }
-};
 
-seedRestaurants();
+  if (restaurant.deliveryServices.includes('Uber Eats')) {
+    const uberEatsRestaurant = uberEatsData.find(
+      (r) => r.name.toLowerCase() === restaurant.name.toLowerCase()
+    );
 
+    if (uberEatsRestaurant) {
+      const dishes = uberEatsRestaurant.menu_items.map((d) => ({
+        name: d.name,
+        price: d.price,
+        promotion: d.promotion,
+        deliveryService: 'Uber Eats',
+        restaurantId: restaurant.id,
+      }));
+
+      await Dish.bulkCreate(dishes);
+    }
+  }
+}
+}
+ createDishes()
  
 
