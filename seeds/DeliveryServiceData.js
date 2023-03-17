5c2977cc88b570a173a85715a5b20c0583d0f4e0
MaximosSarmpas <117605647+MaximosSarmpas@users.noreply.github.com>
2023-03-16 23:18:49 -0400

    Add files via upload
    DeliveryServiceData.js file added

---
+++ seeds/DeliveryServiceData.js
@@ -0,0 +1,11 @@
+const { DeliveryService } = require('../models')
+
+const deliveryServiceData = 
+        [
+          { name: 'Uber Eats', baseURL: 'https://www.ubereats.com/', logoURL: 'https://live.staticflickr.com/21/30786225103_a896d46df9_n.jpg' },
+          { name: 'DoorDash', baseURL: 'https://www.doordash.com/', logoURL: 'https://crystalpng.com/wp-content/uploads/2022/03/doordash-logo-transparent-1.png' }
+        ];
+        console.log('Delivery services created successfully!');
+    
+      const seedDeliveryData = () => Resto.bulkCreate(deliveryServiceData);
+      module.exports = seedDeliveryData;
