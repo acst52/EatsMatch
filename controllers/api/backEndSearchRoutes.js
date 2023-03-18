05902c983c17b03f5dc4d17d94ee87045fb1550b
Ash <acst52@gmail.com>
2023-03-18 18:55:16 -0400

    added extra routes to display resto and dish JSON data @ endpts

--- controllers/api/backEndSearchRoutes.js
+++ controllers/api/backEndSearchRoutes.js
@@ -6,11 +6,31 @@ router.get('/api/restos', async (req, res) => {
     const restos = await Resto.findAll();
     res.json(restos.map((resto) => resto.toJSON()));
 });
+
+// Get resto data to render on localhost:3001/api/resto
+router.get('/resto', async (req, res) => {
+    try {
+        const restos = await Resto.findAll();
+        res.json(restos);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
   
 // Get all dishes
 router.get('/api/dishes', async (req, res) => {
     const dishes = await Dish.findAll();
     res.json(dishes.map((dish) => dish.toJSON()));
+});
+
+// Get dish data to render on localhost:3001/api/dish
+router.get('/dish', async (req, res) => {
+    try {
+        const dishes = await Dish.findAll();
+        res.json(dishes);
+    } catch (error) {
+        res.status(500).json(error);
+    }
 });
 
 module.exports = router;
