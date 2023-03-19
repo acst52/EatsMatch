3d1c2766a5fe2af9ffdc9f02667cde9ffcbbc13d
rnvshi <117702694+rnvshi@users.noreply.github.com>
2023-03-18 22:17:55 -0400

    Update logout.js

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,6 +1,6 @@
 const logout = async () => {
 
-  const response = await fetch('/api/users/logout', {
+  const response = await fetch('/users/logout', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
   });
