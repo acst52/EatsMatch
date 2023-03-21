3ada6a7603d9736f7547da965cc8b2bf2b4ac6ba
Isha Rahman <ishaarahman@gmail.com>
2023-03-20 21:19:28 -0400

    added dish id

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,20 +1,19 @@
 const addToCartHandler = async function (event) { // idk what event is for here
 
     console.log('this has been clicked');
-    const dishPrice = this.previousSibling.value;
-    const dishName = this.parentElement.firstElementChild.value;
+    const dishId = this.parentElement.firstElementChild.value;
 
-    console.log(dishName, dishPrice);
+    console.log(dishId);
 
-    await fetch('/api/cart/add', {
-        method: 'POST',
-        body: JSON.stringify({
-            dishName, dishPrice
-        }),
-        headers: {
-            'Content-Type': 'application/json',
-        }
-    });
+    // await fetch('/api/cart/add', {
+    //     method: 'POST',
+    //     body: JSON.stringify({
+    //         dishName, dishPrice
+    //     }),
+    //     headers: {
+    //         'Content-Type': 'application/json',
+    //     }
+    // });
 
     successMsg.textContent = `Added to cart!`;
     successModal.style.display = 'block'
