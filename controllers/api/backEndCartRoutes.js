cca7540b9ab70d37a897337fb5d13b83642532df
Ash <acst52@gmail.com>
2023-05-23 16:45:29 -0400

    minor changes to cart.js: changed dishName in fetch call to dishID (testing out 'this'), plus added credentials: 'include' to get user's session. also working on new /api/cart/add route

--- controllers/api/backEndCartRoutes.js
+++ controllers/api/backEndCartRoutes.js
@@ -5,21 +5,49 @@ const withAuth = require('../../utils/auth');
 // 5. POST /cart/:id --> this route should handle form subission from menu pg & add selected itrm to user's cart. in this route, use session or cookies to store the user's cart data and redirect user to their cart pg. make sure user auth to be able to add to cart. if user not, redir to login/signup pg
 
 router.post('/cart/add/:id', withAuth, async (req, res) => {
-    const { dish_id } = req.body;
-    try {
-        const dish = await Dish.findByPk(dish_id);
-        const addToCart = await Cart.create({
-            dish_name: dish.dish_name,
-            dish_price: dish.dish_price,
-            // user_id: req.session.user_id,
-            // quantity: 1 // default quant to addToCart is 1
-        });
+	const { dish_id } = req.body;
+	try {
+		const dish = await Dish.findByPk(dish_id);
+		const addToCart = await Cart.create({
+			dish_name: dish.dish_name,
+			dish_price: dish.dish_price,
+			// user_id: req.session.user_id,
+			// quantity: 1 // default quant to addToCart is 1
+		});
 
-        // return updated cart item as JSON
-        return res.status(200).json({ addToCart });
-    } catch (error) {
-        res.status(500).json({ error: 'Server error - unable to add to cart' });
-    }
+		// return updated cart item as JSON
+		return res.status(200).json({ addToCart });
+	} catch (error) {
+		res.status(500).json({ error: 'Server error - unable to add to cart' });
+	}
 });
 
 module.exports = router;
+
+// REWORKING ABOVE ROUTE FOR api/cart/add!!!
+
+router.post('/api/cart/add', withAuth, async (req, res) => {
+	try {
+		// Check if the dish is already in the cart
+		let cartItem = await Cart.findOne({
+			where: { user_id: req.session.user_id, dish_id: req.body.dishName },
+		});
+
+		if (cartItem) {
+			// inc quantity if dish is already in the cart
+			cartItem.quantity += 1;
+			await cartItem.save();
+		} else {
+			// create a cart & add cartItem to it:
+			cartItem = await Cart.create({
+				user_id: req.session.user_id,
+				dish_id: req.body.dishName,
+				quantity: 1,
+			});
+		}
+
+		res.status(200).send('Added to cart');
+	} catch (error) {
+		res.status(500).send('Cannot add to cart. Server error.');
+	}
+});
--- public/js/cart.js
+++ public/js/cart.js
@@ -1,38 +1,42 @@
-const addToCartHandler = async function (event) { // idk what event is for here
+const addToCartHandler = async function (event) {
+	// idk what event is for here
 
-    console.log('this has been clicked');
-    const dishPrice = this.previousSibling.value;
-    const dishName = this.parentElement.firstElementChild.value;
+	console.log('this has been clicked');
+	const dishPrice = this.previousSibling.value;
+	const dishName = this.parentElement.firstElementChild.value;
 
-    console.log(dishName, dishPrice);
+	console.log(dishName, dishPrice);
 
-    await fetch('/api/cart/add', {
-        method: 'POST',
-        body: JSON.stringify({
-            dishName, dishPrice
-        }),
-        headers: {
-            'Content-Type': 'application/json',
-        }
-    });
+	await fetch('/api/cart/add', {
+		method: 'POST',
+		body: JSON.stringify({
+			dishID: this.id,
+			dishPrice,
+		}),
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		credentials: 'include', // session management
+	});
 
-    successMsg.textContent = `Added to cart!`;
-    successModal.style.display = 'block'
-    // add here: add to cart w/ session
-}
+	successMsg.textContent = `Added to cart!`;
+	successModal.style.display = 'block';
+};
 
 const successModal = document.getElementById('success-modal');
 const successMsg = document.getElementById('success-message');
 const closeBtn = document.querySelector('.close');
 
 closeBtn.addEventListener('click', () => {
-    successModal.style.display = 'none';
+	successModal.style.display = 'none';
 });
 window.addEventListener('click', (event) => {
-    if (event.target === successModal) {
-        successModal.style.display = 'none';
-    }
+	if (event.target === successModal) {
+		successModal.style.display = 'none';
+	}
 });
 
+document
+	.querySelector('.add-button')
+	.addEventListener('click', addToCartHandler);
 // document.querySelector('.add-button').addEventListener('click', addToCartHandler);
-document.querySelector('.add-button').addEventListener('click', addToCartHandler);
