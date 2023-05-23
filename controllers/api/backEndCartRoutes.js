6262373a32404a87f1f6a770c0843ee7ea62b7a4
Ash <acst52@gmail.com>
2023-05-23 16:48:57 -0400

    fix /api/cart/add route

--- controllers/api/backEndCartRoutes.js
+++ controllers/api/backEndCartRoutes.js
@@ -4,33 +4,33 @@ const withAuth = require('../../utils/auth');
 
 // 5. POST /cart/:id --> this route should handle form subission from menu pg & add selected itrm to user's cart. in this route, use session or cookies to store the user's cart data and redirect user to their cart pg. make sure user auth to be able to add to cart. if user not, redir to login/signup pg
 
-router.post('/cart/add/:id', withAuth, async (req, res) => {
-	const { dish_id } = req.body;
-	try {
-		const dish = await Dish.findByPk(dish_id);
-		const addToCart = await Cart.create({
-			dish_name: dish.dish_name,
-			dish_price: dish.dish_price,
-			// user_id: req.session.user_id,
-			// quantity: 1 // default quant to addToCart is 1
-		});
+// // OLD ROUTE:
 
-		// return updated cart item as JSON
-		return res.status(200).json({ addToCart });
-	} catch (error) {
-		res.status(500).json({ error: 'Server error - unable to add to cart' });
-	}
-});
+// router.post('/cart/add/:id', withAuth, async (req, res) => {
+// 	const { dish_id } = req.body;
+// 	try {
+// 		const dish = await Dish.findByPk(dish_id);
+// 		const addToCart = await Cart.create({
+// 			dish_name: dish.dish_name,
+// 			dish_price: dish.dish_price,
+// 			// user_id: req.session.user_id,
+// 			// quantity: 1 // default quant to addToCart is 1
+// 		});
 
-module.exports = router;
+// 		// return updated cart item as JSON
+// 		return res.status(200).json({ addToCart });
+// 	} catch (error) {
+// 		res.status(500).json({ error: 'Server error - unable to add to cart' });
+// 	}
+// });
 
-// REWORKING ABOVE ROUTE FOR api/cart/add!!!
+// NEW ROUTE USING DISH_ID, NOT DISHNAME
 
 router.post('/api/cart/add', withAuth, async (req, res) => {
 	try {
 		// Check if the dish is already in the cart
 		let cartItem = await Cart.findOne({
-			where: { user_id: req.session.user_id, dish_id: req.body.dishName },
+			where: { user_id: req.session.user_id, dish_id: req.body.dishId },
 		});
 
 		if (cartItem) {
@@ -41,13 +41,15 @@ router.post('/api/cart/add', withAuth, async (req, res) => {
 			// create a cart & add cartItem to it:
 			cartItem = await Cart.create({
 				user_id: req.session.user_id,
-				dish_id: req.body.dishName,
+				dish_id: req.body.dishId,
 				quantity: 1,
 			});
 		}
 
 		res.status(200).send('Added to cart');
 	} catch (error) {
-		res.status(500).send('Cannot add to cart. Server error.');
+		res.status(500).send('Cannot add to cart. Try again later!');
 	}
 });
+
+module.exports = router;
